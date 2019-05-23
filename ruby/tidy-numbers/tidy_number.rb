ecd49296f683398f7fbcce5b1b46262bf5d8b21e
Domenico <domenico.angilletta@gmail.com>
2019-05-23 21:24:39 +0200

    Remove unnecessary to_s

--- ruby/tidy-numbers/tidy_number.rb
+++ ruby/tidy-numbers/tidy_number.rb
@@ -9,7 +9,7 @@ class TidyNumber
                 return n if index == -1
                 
                 decreased_prefix = n[0, index].to_i - 1
-                return self.result(decreased_prefix.to_s) + '9' * (n.length-index)
+                return self.result(decreased_prefix) + '9' * (n.length-index)
         end
 
         def self.first_incorrect_index(n)
